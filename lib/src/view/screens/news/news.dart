4c44d7dbcb88cb0866615eb3d01adc58a1568b61
MahnoorWaheed <mahnoorwaheed8395@gmail.com>
2023-02-09 14:46:05 +0500

    adding section

---
+++ lib/src/view/screens/news/news.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class News extends StatefulWidget {
+  const News({super.key});
+
+  @override
+  State<News> createState() => _NewsState();
+}
+
+class _NewsState extends State<News> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.pink,
+    );
+  }
+}
